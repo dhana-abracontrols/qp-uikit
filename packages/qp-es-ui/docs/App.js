76287da4e61f9335448ed98ffbbb724d491be5ad
dhana-abracontrols <dhana.rama@abracontrols.com>
2021-05-17 15:15:47 -0500

    updated-05172021

--- packages/qp-es-ui/docs/App.js
+++ packages/qp-es-ui/docs/App.js
@@ -17,7 +17,7 @@ class App extends Component {
     // API_KEY=API_KEY. If in production, API_KEY is set to ''
 
     // This allows devs to skip copy pasting a key into the box every reload.
-    this.state = { API_KEY : 'temp'}
+    this.state = { API_KEY }
     this.updateKey = this.updateKey.bind(this)
   }
 
